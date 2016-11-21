c94b67ecab1046a436f733f717080bf232ee0449
Dong-Gun Chung <chungdk1117@gmail.com>
2016-11-22 00:49:20 +0900

    client.c file is updated
    Signed-off-by: Dong-Gun Chung <chungdk1117@gmail.com>

--- client.c
+++ client.c
@@ -7,15 +7,22 @@
 #include <sys/socket.h>
 #include <malloc.h>
 
+#define GRT_LOG 100
+#define SEND_LOG 101
+#define FAIL_SEND 102
+#define EXIT_CMD 900
+#define EXIT_PRC 901
+#define MAX 32
+
 void error_handling(char *message);
 
-
+char input[MAX]; // string that will send to server
 
 int main(int argc, char *argv[])
 {
 	int sock;
 	struct sockaddr_in serv_addr;
-	char message[30];
+	char message[MAX]; // array that will store string from server 
 	int str_len;
 
 	if (argc != 3) {
@@ -35,12 +42,17 @@ int main(int argc, char *argv[])
 	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) /* 서버로 연결 요청 */
 		error_handling("connect() error!");
 
+ 	scanf("%s", input);
+	input[strlen(input)] = '\0';
+	write(sock, input , strlen(input)+1); // send input string to server
+
+
 	str_len = read(sock, message, sizeof(message) - 1); /* 데이터 수신 */
 	if (str_len == -1)
 		error_handling("read() error!");
 
 	message[str_len] = '\0';
-	printf("Message from server : %s\n", message);
+	printf("Message from server : %s\n", message); // print msg from server
 
 	close(sock); /* 연결 종료 */
 
